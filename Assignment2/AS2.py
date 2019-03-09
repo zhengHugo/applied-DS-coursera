8639b6286217b38717e7bb158a99468349207d93
zhengHugo <zhenghugo515@gmail.com>
2019-03-09 14:09:41 +0800

    Finish question 1

--- Assignment2/AS2.py
+++ Assignment2/AS2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Draft/Assignment2/olympics.csv', index_col=0, skiprows=1, encoding='utf-8')
+df = pd.read_csv('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment2/olympics.csv', index_col=0, skiprows=1, encoding='utf-8')
 
 for col in df.columns:
     if col[:2]=='01':
@@ -18,4 +18,17 @@ df.index = names_ids.str[0] # the [0] element is the country name (new index)
 df['ID'] = names_ids.str[1].str[:3] # the [1] element is the abbreviation or ID (take first 3 characters from that)
 
 df = df.drop('Totals')
-print(df.head())
+# print(df.head())
+
+# Which country has won the most gold medals in summer games?
+# This function should return a single string value.
+def answer_one():
+    max_country = ''
+    max = 0
+    for index, row in df.iterrows():
+        if row['Gold'] > max:
+            max = row['Gold']
+            max_country = index
+    return max_country
+
+print(answer_one())
