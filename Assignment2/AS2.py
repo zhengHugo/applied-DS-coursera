f5921927a805297ec934121818f843139efff44d
zhengHugo <zhenghugo515@gmail.com>
2019-03-09 14:49:05 +0800

    Finish question 2

--- Assignment2/AS2.py
+++ Assignment2/AS2.py
@@ -14,8 +14,12 @@ for col in df.columns:
 
 names_ids = df.index.str.split('\\s\\(') # split the index by '('
 
-df.index = names_ids.str[0] # the [0] element is the country name (new index) 
-df['ID'] = names_ids.str[1].str[:3] # the [1] element is the abbreviation or ID (take first 3 characters from that)
+# the [0] element is the country name (new index) 
+df.index = names_ids.str[0] 
+
+# the [1] element is the abbreviation or ID 
+# (take first 3 characters from that)
+df['ID'] = names_ids.str[1].str[:3] 
 
 df = df.drop('Totals')
 # print(df.head())
@@ -31,4 +35,18 @@ def answer_one():
             max_country = index
     return max_country
 
-print(answer_one())
+# print(answer_one())
+
+# Which country had the biggest difference between their summer and winter 
+# gold medal counts?
+# This function should return a single string value
+def answer_two():
+    rt_country = ''
+    max_diff = 0
+    for index, row in df.iterrows():
+        if abs(row['Gold'] - row['Gold.1']) > max_diff:
+            max_diff = abs(row['Gold'] - row['Gold.1'])
+            rt_country = index
+    return rt_country
+
+# print(answer_two())
