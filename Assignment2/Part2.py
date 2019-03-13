f7b6982e32e7cb100df2075c62bef6b85fa7fe4d
zhengHugo <zhenghugo515@gmail.com>
2019-03-13 11:01:51 +0800

    Finish question 6

--- Assignment2/Part2.py
+++ Assignment2/Part2.py
@@ -11,14 +11,26 @@ census_df = pd.read_csv('C:/Users/11796/Documents/Study_Materials/Applied_Data_S
 '''
 def answer_five():
     states = census_df['STNAME'].unique()
-    # print(states)
-    # print(census_df['STNAME'])
     county_num = pd.Series([0] * len(states), index = states)
     for country in census_df.iterrows():
         if country[1]['SUMLEV'] == 40:
             continue
         county_num[country[1]['STNAME']] += 1
     return county_num.idxmax()
-print(answer_five())
 
+# print(answer_five())
 
+'''
+    Only look at the three most populous counties for each state 
+    What are the three most populous states (in order of highest population to
+    lowest population)? Use CENSUS2010POP.
+'''
+def answer_six():
+    counties_df = census_df.where(census_df['SUMLEV'] == 50)
+    top_counties_df = counties_df.sort_values(by = 'CENSUS2010POP', ascending  = False).groupby('STNAME').head(3)
+    ans = top_counties_df.groupby('STNAME').sum().sort_values(by = 'CENSUS2010POP', ascending = False).head(3).index.tolist()
+    return ans
+
+# print (answer_six())
+# Reference: 
+# https://github.com/Qian-Han/coursera-Applied-Data-Science-with-Python/blob/master/Introduction-to-Data-Science-in-Python/week2/week2_Assignment.ipynb
