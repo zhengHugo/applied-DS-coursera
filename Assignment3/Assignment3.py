5e1a5676973e5a40624488d31e224ec2388001da
zhengHugo <zhenghugo515@gmail.com>
2019-03-21 23:02:30 +0800

    Finish question2

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 
 def answer_one():
+    # ----------------------------------------------------------------------------------
+    # Read energy
     energy = pd.read_excel('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/Energy Indicators.xls',
                            skiprows=17, skipfooter=38)
     columns_keep = ['Unnamed: 1', 'Petajoules', 'Gigajoules', '%']
@@ -20,6 +22,8 @@ def answer_one():
     energy['Country'] = energy['Country'].str.replace(
         r'\(.*\)', '', regex=True).str.strip()
 
+    # ----------------------------------------------------------------------------------
+    # Read GDP
     GDP = pd.read_csv(
         'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/world_bank.csv', skiprows=4)
     GDP['Country Name'] = GDP['Country Name'].replace({
@@ -33,11 +37,16 @@ def answer_one():
                        '2009', '2010', '2011', '2012', '2013', '2014', '2015', ]
     GDP = GDP[columns_to_keep]
 
+    # ----------------------------------------------------------------------------------
+    # Read ScimEn
     ScimEn = pd.read_excel(
         'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/scimagojr-3.xlsx')
 
     # Use only the top 15 countries
     ScimEn = ScimEn[:15]
+
+    # ----------------------------------------------------------------------------------
+    # merge dataframes
     df = pd.merge(GDP, energy, how='inner',
                   left_on='Country Name', right_on='Country')
     df = pd.merge(df, ScimEn, how='inner',
@@ -48,5 +57,67 @@ def answer_one():
     df = df[columns_to_keep]
     print(df)
 
+# answer_one()
 
-answer_one()
+
+def answer_two():
+    # Read energy
+    energy = pd.read_excel('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/Energy Indicators.xls',
+                           skiprows=17, skipfooter=38)
+    columns_keep = ['Unnamed: 1', 'Petajoules', 'Gigajoules', '%']
+    energy = energy[columns_keep]
+    columns = ['Country', 'Energy Supply',
+               'Energy Supply per Capita', '% Renewable']
+    energy.columns = columns
+    energy.iloc[:, 1:4] = energy.iloc[:, 1:4].replace(
+        '...', np.NaN).apply(pd.to_numeric)
+    energy['Energy Supply'] = energy['Energy Supply'] * 1000000
+    energy['Country'] = energy['Country'].replace(
+        {'Republic of Korea': 'South Korea', 'United States of America': 'United States', 'United Kingdom of Great Britain and Northern Ireland': 'United Kingdom', 'China, Hong Kong Special Administrative Region': 'Hong Kong'})
+
+    # Remove thing in () and revome abundant spaces
+    energy['Country'] = energy['Country'].str.replace(
+        r'\(.*\)', '', regex=True).str.strip()
+
+    # ----------------------------------------------------------------------------------
+    # Read GDP
+    GDP = pd.read_csv(
+        'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/world_bank.csv', skiprows=4)
+    GDP['Country Name'] = GDP['Country Name'].replace({
+        'Korea, Rep.': 'South Korea', 'Iran, Islamic Rep.': 'Iran', 'Hong Kong SAR, China': 'Hong Kong'
+    })
+    # with pd.option_context('display.max_rows', None):
+    # print(GDP['Country Name'])
+
+    # Use last ten years of GDP data
+    columns_to_keep = ['Country Name', '2006', '2007', '2008',
+                       '2009', '2010', '2011', '2012', '2013', '2014', '2015', ]
+    GDP = GDP[columns_to_keep]
+    GDP.columns = ['Country', '2006', '2007', '2008',
+                   '2009', '2010', '2011', '2012', '2013', '2014', '2015', ]
+
+    # ----------------------------------------------------------------------------------
+    # Read ScimEn
+    ScimEn = pd.read_excel(
+        'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/scimagojr-3.xlsx')
+
+    # ----------------------------------------------------------------------------------
+    # merge dataframes
+    df1 = pd.merge(GDP, energy, how='inner',
+                   left_on='Country', right_on='Country')
+    df1 = pd.merge(df1, ScimEn, how='inner',
+                   left_on='Country', right_on='Country')
+    df1 = df1.set_index('Country')
+    df2 = pd.merge(GDP, energy, how='outer',
+                   left_on='Country', right_on='Country')
+    df2 = pd.merge(df2, ScimEn, how='outer',
+                   left_on='Country', right_on='Country')
+    df2 = df2.set_index('Country')
+    columns_to_keep = ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index',
+                       'Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+    df1 = df1[columns_to_keep]
+    df2 = df2[columns_to_keep]
+    return len(df2) - len(df1)
+
+
+print(answer_two())
