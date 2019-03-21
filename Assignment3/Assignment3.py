1ba6678356e50193eb158b3cf756f8036d0351d6
zhengHugo <zhenghugo515@gmail.com>
2019-03-21 17:28:07 +0800

    Finish question1

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -15,10 +15,10 @@ def answer_one():
     energy['Energy Supply'] = energy['Energy Supply'] * 1000000
     energy['Country'] = energy['Country'].replace(
         {'Republic of Korea': 'South Korea', 'United States of America': 'United States', 'United Kingdom of Great Britain and Northern Ireland': 'United Kingdom', 'China, Hong Kong Special Administrative Region': 'Hong Kong'})
+
+    # Remove thing in () and revome abundant spaces
     energy['Country'] = energy['Country'].str.replace(
-        r'\(.*\)', '', regex=True)
-    # with pd.option_context('display.max_rows', None):
-    # print(energy['Country'])
+        r'\(.*\)', '', regex=True).str.strip()
 
     GDP = pd.read_csv(
         'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/world_bank.csv', skiprows=4)
@@ -27,15 +27,26 @@ def answer_one():
     })
     # with pd.option_context('display.max_rows', None):
     # print(GDP['Country Name'])
+
+    # Use last ten years of GDP data
     columns_to_keep = ['Country Name', '2006', '2007', '2008',
                        '2009', '2010', '2011', '2012', '2013', '2014', '2015', ]
     GDP = GDP[columns_to_keep]
-    print(GDP.columns)
 
     ScimEn = pd.read_excel(
         'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment3/scimagojr-3.xlsx')
+
+    # Use only the top 15 countries
     ScimEn = ScimEn[:15]
-    print(ScimEn)
+    df = pd.merge(GDP, energy, how='inner',
+                  left_on='Country Name', right_on='Country')
+    df = pd.merge(df, ScimEn, how='inner',
+                  left_on='Country Name', right_on='Country')
+    df = df.set_index('Country Name')
+    columns_to_keep = ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index',
+                       'Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+    df = df[columns_to_keep]
+    print(df)
 
 
 answer_one()
