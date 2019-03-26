322d7044fd641379ca995bfbb141dc7ef1747656
zhengHugo <zhenghugo515@gmail.com>
2019-03-27 01:11:41 +0800

    Finish Assigment4 function get_list_of_university_towns()

---
+++ Assignment4/Assignment4.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import ttest_ind
+
+# Use this dictionary to map state names to two letter acronyms
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina',
+          'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+
+
+def get_list_of_university_towns():
+    '''
+        Returns a DataFrame of towns and the states they are in from the 
+        university_towns.txt list. The format of the DataFrame should be:
+        DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
+        columns=["State", "RegionName"]  )
+
+        The following cleaning needs to be done:
+
+        1. For "State", removing characters from "[" to the end.
+        2. For "RegionName", when applicable, removing every character from " (" to the end.
+        3. Depending on how you read the data, you may need to remove newline character '\n'. 
+    '''
+
+    with open('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment4/university_towns.txt') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    state_town = []
+    for line in content:
+        if '[edit]' in line:
+            state = line[:-6]
+        elif '(' in line:
+            town = line.split('(')[0].strip()
+            state_town.append([state, town])
+        elif ',' in line:
+            # print(line)
+            # two lines are in a improper format, add manually
+            town = line.split(',')[0].strip()
+            state_town.append([state, town])
+    ans = pd.DataFrame(state_town, columns=['State', 'RegionName'])
+    return ans
