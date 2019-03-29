1e6177ffb6e47f8f5a3df8637141082ab1a48508
zhengHugo <zhenghugo515@gmail.com>
2019-03-29 19:30:46 +0800

    Finish function get_recession_end() and get_recession bottom()

--- Assignment4/Assignment4.py
+++ Assignment4/Assignment4.py
@@ -9,16 +9,16 @@ states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada'
 
 def get_list_of_university_towns():
     '''
-        Returns a DataFrame of towns and the states they are in from the 
+        Returns a DataFrame of towns and the states they are in from the
         university_towns.txt list. The format of the DataFrame should be:
-        DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
+        DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ],
         columns=["State", "RegionName"]  )
 
         The following cleaning needs to be done:
 
         1. For "State", removing characters from "[" to the end.
         2. For "RegionName", when applicable, removing every character from " (" to the end.
-        3. Depending on how you read the data, you may need to remove newline character '\n'. 
+        3. Depending on how you read the data, you may need to remove newline character '\n'.
     '''
 
     with open('C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment4/university_towns.txt') as f:
@@ -53,3 +53,41 @@ def get_recession_start():
         if df.iloc[i][1] > df.iloc[i+1][1] and df.iloc[i+1][1] > df.iloc[i+2][1]:
             recessions_start.append(df.iloc[i][0])
     return recessions_start[0]
+
+
+def get_recession_end():
+    '''
+        Returns the year and quarter of the recession end time as a
+        string value in a format such as 2005q3
+    '''
+    df = pd.read_excel(
+        'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment4/gdplev.xls', skiprows=7)
+    df = df[['Unnamed: 4', 'Unnamed: 5']]
+    df.columns = ['Quarters', 'GDP']
+    df = df.iloc[212:]
+    recessions_end = []
+    for i in range(len(df) - 3):
+        if df.iloc[i][1] > df.iloc[i+1][1] > df.iloc[i+2][1] and df.iloc[i+2][1] < df.iloc[i+3][1] < df.iloc[i+4][1]:
+            recessions_end.append(df.iloc[i+4][0])
+    return recessions_end[0]
+
+
+def get_recession_bottom():
+    '''
+        Returns the year and quarter of the recession bottom time as a 
+        string value in a format such as 2005q3
+    '''
+
+    df = pd.read_excel(
+        'C:/Users/11796/Documents/Study_Materials/Applied_Data_Science/Assignments/Assignment4/gdplev.xls', skiprows=7)
+    df = df[['Unnamed: 4', 'Unnamed: 5']]
+    df.columns = ['Quarters', 'GDP']
+    df = df.iloc[212:]
+    recessions_bottom = []
+    for i in range(len(df) - 3):
+        if df.iloc[i][1] > df.iloc[i+1][1] > df.iloc[i+2][1] and df.iloc[i+2][1] < df.iloc[i+3][1] < df.iloc[i+4][1]:
+            recessions_bottom.append(df.iloc[i+2][0])
+    return recessions_bottom[0]
+
+
+print(get_recession_bottom())
