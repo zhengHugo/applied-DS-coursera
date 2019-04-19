4953da62552e3c0704644ff0fde8a8e132c0692d
zhengHugo <zhenghugo515@gmail.com>
2019-04-19 15:36:09 +0800

    Add legends, delete frames

--- Assignments_course2/Assignment2/AS2.py
+++ Assignments_course2/Assignment2/AS2.py
@@ -41,15 +41,38 @@ maxT_ave = maxT_ave.reset_index()
 minT_ave_15 = minT_ave_15.reset_index()
 maxT_ave_15 = maxT_ave_15.reset_index()
 
+# Get points when extrem temperature in 2015 exceed the last ten years
 broke_min = minT_ave_15[minT_ave_15['Data_Value']
                         < minT_ave['Data_Value']].index.tolist()
 broke_max = maxT_ave_15[maxT_ave_15['Data_Value']
-                        < maxT_ave['Data_Value']].index.tolist()
-print(broke_max)
-print(broke_min)
+                        > maxT_ave['Data_Value']].index.tolist()
 
+plt.figure()
+# Plot lines of temperature from 2005-2014
+plt.plot(minT_ave['Data_Value'], 'b', label='High Temperatures')
+plt.plot(maxT_ave['Data_Value'], 'r', label='Low Temperatures')
+# Plot scatter points in 2015
+plt.scatter(
+    broke_min, minT_ave_15['Data_Value'].iloc[broke_min], s=20, c='k', alpha=0.5, label='Broken Min')
+plt.scatter(
+    broke_max, maxT_ave_15['Data_Value'].iloc[broke_max], s=20, c='g', alpha=0.5, label='Broken Max')
+plt.xlabel('Month')
+plt.ylabel('Temperature (Tenths of Degrees C)')
+plt.title('Extreme Temperatures of 2015 against 2005-2014\n in Ann Arbor, Michigan, United States')
+# Fill between the two lines with grey
+plt.gca().fill_between(range(len(minT_ave)),
+                       minT_ave['Data_Value'], maxT_ave['Data_Value'], facecolor='grey', alpha=0.3)
+# Trim the axes
+plt.gca().axis([-5, 370, -330, 350])
+# Show legends and cut the frame
+plt.legend(frameon=False)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
 
-# plt.figure()
-# plt.plot(minT_ave['Data_Value'], 'b', alpha=0.5, label='High Temperatures')
-# plt.plot(maxT_ave['Data_Value'], 'r', alpha=0.5, label='Low Temperatures')
-# plt.show()
+a = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]
+b = [i+15 for i in a]
+
+Month_name = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+              'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+plt.xticks(b, Month_name)
+plt.show()
