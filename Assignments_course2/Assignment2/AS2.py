7d3c9a7c6b14bc5878b6ef2254caaac13d651f2a
zhengHugo <zhenghugo515@gmail.com>
2019-04-19 15:03:39 +0800

    Getting all indecies for the scatter plot

--- Assignments_course2/Assignment2/AS2.py
+++ Assignments_course2/Assignment2/AS2.py
@@ -14,23 +14,42 @@ df['Month'] = df['Date'].str[5:7]
 df['Day'] = df['Date'].str[8:10]
 df['Month-Day'] = df['Date'].str[5:10]
 
+# trim
+df = df[(df['Month-Day'] != '02-29')]
+
 # Separate the data by min temperature and max temperature
 df_minT = df[(df['Element'] == 'TMIN')]
 df_maxT = df[(df['Element'] == 'TMAX')]
+
+# Get 2015 data
+df_minT_15 = df_minT[(df_minT['Year'] == '2015')]
+df_maxT_15 = df_maxT[(df_maxT['Year'] == '2015')]
 # Cut out the year 2015 since we only need 2005-2014
 df_minT = df_minT[(df_minT['Year'] != '2015')]
 df_maxT = df_maxT[(df_maxT['Year'] != '2015')]
 
+# Get the area temperature by averaging data from each station
+minT_ave = df_minT.groupby('Month-Day')['Data_Value'].agg(np.mean)
+maxT_ave = df_maxT.groupby('Month-Day')['Data_Value'].agg(np.mean)
+minT_ave_15 = df_minT_15.groupby('Month-Day')['Data_Value'].agg(np.mean)
+maxT_ave_15 = df_maxT_15.groupby('Month-Day')['Data_Value'].agg(np.mean)
 
-minT_ave = df_minT.groupby(
-    'Month-Day')['Data_Value'].agg(np.mean)
-maxT_ave = df_maxT.groupby(
-    'Month-Day')['Data_Value'].agg(np.mean)
+# Reset the index from Month-Day for the convenience of labling the
+# x-axis in the plot
+minT_ave = minT_ave.reset_index()
+maxT_ave = maxT_ave.reset_index()
+minT_ave_15 = minT_ave_15.reset_index()
+maxT_ave_15 = maxT_ave_15.reset_index()
+
+broke_min = minT_ave_15[minT_ave_15['Data_Value']
+                        < minT_ave['Data_Value']].index.tolist()
+broke_max = maxT_ave_15[maxT_ave_15['Data_Value']
+                        < maxT_ave['Data_Value']].index.tolist()
+print(broke_max)
+print(broke_min)
 
 
-print(type(minT_ave))
-print(type(maxT_ave))
-print(minT_ave['value'])
 # plt.figure()
-# plt.plot(minT_ave, 'y')
+# plt.plot(minT_ave['Data_Value'], 'b', alpha=0.5, label='High Temperatures')
+# plt.plot(maxT_ave['Data_Value'], 'r', alpha=0.5, label='Low Temperatures')
 # plt.show()
