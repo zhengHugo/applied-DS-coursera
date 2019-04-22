d93e6a58d8ab485c79f0523e096fad31187a4ac1
Hugo Zheng <hugozheng@Hugos-MacBook-Pro.local>
2019-04-23 01:19:21 +0800

    Let bar color be based on the value

--- Assignments_course2/Assignment3/AS3.py
+++ Assignments_course2/Assignment3/AS3.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-np.random.seed(12345)
+np.random.seed(24201)
 
 df = pd.DataFrame([np.random.normal(32000, 200000, 3650),
                    np.random.normal(43000, 100000, 3650),
@@ -16,15 +16,24 @@ year_std = df.std(axis=1)
 confidence = 0.95
 yerr = year_std / np.sqrt(df.shape[1]) * \
     stats.t.ppf((1+confidence)/2.0, df.shape[1]-1)
-# print(year_std)
-# print(yerr)
 
 plt.figure()
 
-bars = plt.bar(range(df.shape[0]), year_avg,
-               yerr=yerr, alpha=0.6, color='rgby')
-
+y = np.array([0, 1])
+plot = plt.scatter(y, y, c=y, cmap='RdBu')
+plt.clf()
+plt.colorbar(plot)
 threshold = 42000
+cmap = plt.cm.get_cmap('RdBu')
+colors = (year_avg - threshold)/yerr/2 + 0.5
+colors[colors > 1] = 1
+colors[colors < 0] = 0
+print(colors)
+colors = cmap(colors)
+
+bars = plt.bar(range(df.shape[0]), year_avg,
+               yerr=yerr, alpha=0.6, color=colors)
+
 plt.axhline(y=threshold, color='grey')
 
 plt.xticks(range(df.shape[0]), ['1992', '1993', '1994', '1995'], alpha=0.8)
