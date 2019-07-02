50249d9f9b92635fd887a5708f21729586146170
zhengHugo <zhenghugo515@gmial.com>
2019-07-02 19:17:27 +0800

    Finish question 2

--- Assignments_course4/Week3/AS3.py
+++ Assignments_course4/Week3/AS3.py
@@ -1,3 +1,4 @@
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
@@ -7,9 +8,22 @@ spam_data['target'] = np.where(spam_data['target'] == 'spam', 1, 0)
 x_train, X_test, y_train, y_test = train_test_split(
     spam_data['text'], spam_data['target'], random_state=0)
 
+
 # What percentage of the documents in spam_data are spam?
-
-
 def answer_one():
     ans = len(spam_data[spam_data['target'] == 1]) / len(spam_data) * 100
     return ans
+
+
+# Fit the training data X_train using a Count Vectorizer with default parameters.
+# What is the longest token in the vocabulary?
+
+
+def answer_two():
+    X_train = spam_data['text']
+    vect = CountVectorizer().fit(X_train)
+    sortedWords = sorted(vect.get_feature_names(), key=len)
+    return sortedWords[-1]
+
+
+print(answer_two())
