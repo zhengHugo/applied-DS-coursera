0ed351f6aadcac486489174caf8ca1382bfb5bbe
zhengHugo <zhenghugo515@gmial.com>
2019-07-03 14:36:19 +0800

    Finish question 3

--- Assignments_course4/Week3/AS3.py
+++ Assignments_course4/Week3/AS3.py
@@ -1,11 +1,13 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import roc_auc_score
 import pandas as pd
 import numpy as np
 
 spam_data = pd.read_csv('Assignments_course4/Week3/spam.csv')
 spam_data['target'] = np.where(spam_data['target'] == 'spam', 1, 0)
-x_train, X_test, y_train, y_test = train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(
     spam_data['text'], spam_data['target'], random_state=0)
 
 
@@ -17,13 +19,19 @@ def answer_one():
 
 # Fit the training data X_train using a Count Vectorizer with default parameters.
 # What is the longest token in the vocabulary?
-
-
 def answer_two():
-    X_train = spam_data['text']
-    vect = CountVectorizer().fit(X_train)
+    data = spam_data['text']
+    vect = CountVectorizer().fit(data)
     sortedWords = sorted(vect.get_feature_names(), key=len)
     return sortedWords[-1]
 
 
-print(answer_two())
+# Fit and transform the training data X_train using a Count Vectorizer with default parameters.
+# Next, fit a fit a multinomial Naive Bayes classifier model with smoothing alpha=0.1. Find the area under the curve (AUC) score using the transformed test data.
+def answer_three():
+    vect = CountVectorizer().fit(X_train)
+    X_train_vectorized = vect.transform(X_train)
+    model = MultinomialNB(alpha=0.1)
+    model.fit(X_train_vectorized, y_train)
+    predictions = model.predict(vect.transform(X_test))
+    return roc_auc_score(y_test, predictions)
