824c6cf159531dc35f9245d2f2fdae94022cd4c3
zhengHugo <zhenghugo515@gmial.com>
2019-07-11 15:36:14 +0800

    Start course 5 week 1, finish question 1

--- Assignments_course5/Week1/AS1.py
+++ Assignments_course5/Week1/AS1.py
@@ -1,2 +1,62 @@
 import networkx as nx
-nx.test()
+import pandas as pd
+import numpy as np
+from networkx.algorithms import bipartite
+
+
+# This is the set of employees
+employees = set(['Pablo',
+                 'Lee',
+                 'Georgia',
+                 'Vincent',
+                 'Andy',
+                 'Frida',
+                 'Joan',
+                 'Claude'])
+
+# This is the set of movies
+movies = set(['The Shawshank Redemption',
+              'Forrest Gump',
+              'The Matrix',
+              'Anaconda',
+              'The Social Network',
+              'The Godfather',
+              'Monty Python and the Holy Grail',
+              'Snakes on a Plane',
+              'Kung Fu Panda',
+              'The Dark Knight',
+              'Mean Girls'])
+
+
+# you can use the following function to plot graphs
+# make sure to comment it out before submitting to the autograder
+def plot_graph(G, weight_name=None):
+    '''
+    G: a networkx G
+    weight_name: name of the attribute for plotting edge weights (if G is weighted)
+    '''
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    pos = nx.spring_layout(G)
+    edges = G.edges()
+    weights = None
+
+    if weight_name:
+        weights = [int(G[u][v][weight_name]) for u, v in edges]
+        labels = nx.get_edge_attributes(G, weight_name)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        nx.draw_networkx(G, pos, edges=edges, width=weights)
+    else:
+        nx.draw_networkx(G, pos, edges=edges)
+
+    plt.show()
+
+
+def answer_one():
+    G = nx.read_edgelist(
+        'Assignments_course5/Week1/Employee_Movie_Choices.txt', delimiter="\t")
+    return G.edges
+
+
+print(answer_one())
--- Assignments_course5/Week1/Note1.md
+++ Assignments_course5/Week1/Note1.md
@@ -16,4 +16,15 @@ A pair of nodes can have different types of relationships simultaneously
 See the notebook
 - Adding node and edge attributes
 - Accessing node attributes
-- 
+
+## Bipartite Graphs
+`from networkx.algorithms import bipartite`
+- Checking if a graph is bipartite
+- Checking if a set of nodes is a bipartition of a graph
+- Getting each set of nodes of a bipartite graph
+### Projected Graphs
+**L-Bipartite graph**: Network of nodes in group L, where a pair of nodes is connected if they have a common neighbor in R in the bipartite graph
+
+Similar definition for R-Bipartite graph projection
+
+**L-Bipartite weighted graph projection**: An L-Bipartite graph projection with weights on the edges that are proportional to the number of common neighbors between the nodes
