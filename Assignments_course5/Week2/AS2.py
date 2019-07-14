d310bdb23e317dd2f652ee1967424d7dc33213d1
zhengHugo <zhenghugo515@gmial.com>
2019-07-14 14:05:08 +0800

    Finish question 2

--- Assignments_course5/Week2/AS2.py
+++ Assignments_course5/Week2/AS2.py
@@ -5,3 +5,8 @@ def answer_one():
     G = nx.read_edgelist('Assignments_course5/Week2/email_network.txt',
                          delimiter='\t', data=[('time', int)], create_using=nx.MultiDiGraph())
     return G
+
+
+def answer_two():
+    G = answer_one()
+    return len(G.nodes()), len(G.edges())
